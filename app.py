c628dd06d6b04747833100def91a06bf1c6354e9
ahmedelasmai <ahmed.elasmai@outlook.com>
2024-03-31 21:53:25 +0100

    finished the basics of the app

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ list = []
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        task = request.form['task']
-        list.append(task)
-    
+        if 'task' in request.form:
+            task = request.form['task']
+            list.append(task)
+
+        if 'delete' in request.form:
+            item_to_delete = request.form['delete']
+            list.remove(item_to_delete)
+
     return render_template('index.html',list=list)
-#test
-#test
+
+
